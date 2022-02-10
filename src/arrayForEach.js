ac2b17573b996c8e1929c97f4ef231935c55c887
Fikri Khairul Shaleh <fikriks130@gmail.com>
2022-02-10 16:07:40 +0700

    array foreach :rocket:

---
+++ src/arrayForEach.js
@@ -0,0 +1,12 @@
+/*
+    Array ForEach
+*/
+
+// ES6
+const mySkills = ['Web Development', 'Copywriter', 'DevOps'];
+
+mySkills.push("Design");
+
+mySkills.forEach((skill) => {
+    console.log(`Keahlian saya adalah : ${skill}`);
+})

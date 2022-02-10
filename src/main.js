13c5aded17cb1678faeb2751fcba8661df815d58
Fikri Khairul Shaleh <fikriks130@gmail.com>
2022-02-10 15:19:48 +0700

    var dan cost :rocket:

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 console.log("Hello World!");
+
+// ES5
+var myName = "Fikri Khairul Shaleh";
+
+myName = "Fikri KS";
+console.log("Var pada ES5 :"+myName);
+
+// ES6
+const myCar = "Lamborghini";
+// myCar = "Ayla"; -> error
+
+console.log("Const pada ES6 : "+myCar);

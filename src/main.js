5f3a3bf6520431b7566bec1f2fa54d8638b8add2
Fikri Khairul Shaleh <fikriks130@gmail.com>
2022-02-10 15:41:17 +0700

    string concatenation :rocket:

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 console.log("Hello World!");
 
+/*
+    Variable
+*/
+
 // ES5
 var myName = "Fikri Khairul Shaleh";
 
 myName = "Fikri KS";
-console.log("Var pada ES5 :"+myName);
+console.log(`Var pada ES5 : ${myName}`);
 
 // ES6
 // Const tidak bisa diubah
@@ -13,9 +17,23 @@ console.log("Var pada ES5 :"+myName);
 const myCar = "Lamborghini";
 // myCar = "Ayla"; -> error
 
-console.log("Const pada ES6 : "+myCar);
+console.log(`Const pada ES6 : ${myCar}`);
 
 let myClothes = "Wadezig"; 
 myClothes = "AHHA";
 
-console.log("Let pada ES6 : "+myClothes);
+console.log(`Let pada ES6 : ${myClothes}`);
+
+/*
+    String Concatenation
+*/
+
+const user = document.getElementById('user');
+const user1 = document.getElementById('user1');
+const user_logged = "Fikri";
+
+// ES5
+user.innerHTML = "Hi, " + user_logged + " Apa Kabar?";
+
+// ES6
+user1.innerHTML = `Hi, ${user_logged} Apa Kabar?`;

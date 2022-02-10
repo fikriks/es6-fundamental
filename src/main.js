0b692a16b52e281ebc30926bf081d2a904d12dc7
Fikri Khairul Shaleh <fikriks130@gmail.com>
2022-02-10 15:32:14 +0700

    let :rocket:

--- src/main.js
+++ src/main.js
@@ -7,7 +7,15 @@ myName = "Fikri KS";
 console.log("Var pada ES5 :"+myName);
 
 // ES6
+// Const tidak bisa diubah
+// Let bisa diubah
+
 const myCar = "Lamborghini";
 // myCar = "Ayla"; -> error
 
 console.log("Const pada ES6 : "+myCar);
+
+let myClothes = "Wadezig"; 
+myClothes = "AHHA";
+
+console.log("Let pada ES6 : "+myClothes);
